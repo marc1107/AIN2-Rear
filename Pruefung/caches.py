946dc2a2cf13d7a8e525b51155c82195df5ccc7d
marc1107 <m.bohner99@gmail.com>
2022-07-04 16:07:44 +0200

    Caches

--- Pruefung/caches.py
+++ Pruefung/caches.py
@@ -17,11 +17,11 @@ def directMapped():
 
     print("Blocknummer: %d\t\t\tBerechnung: floor(Adresse / Blockgröße) = floor(%d / %d)" % (
         blocknummer, adresse, blocksize))
+    print("Offset (in Bytes): %d\t\tBerechnung: Adresse mod Blockgröße = %d mod %d" % (offset, adresse, blocksize))
     print(
         "Blockindex: %d\t\t\t\tBerechnung: Blocknummer mod Blockanzahl = %d mod %d" % (blockindex, blocknummer, anzahl))
     print(
         "Tag: %d\t\t\t\t\t\tBerechnung: floor(Blocknummer / Blockanzahl) = floor(%d / %d)" % (tag, blocknummer, anzahl))
-    print("Offset (in Bytes): %d\t\tBerechnung: Adresse mod Blockgröße = %d mod %d" % (offset, adresse, blocksize))
 
 
 def xWay():
@@ -38,8 +38,11 @@ def xWay():
     tag = math.floor(blocknummer / anzahlsets)
     offset = adresse % blocksize
 
+    print("Gilt für alle Cache-Level:")
     print("Blocknummer: %d\t\t\t\tBerechnung: floor(Adresse / Blockgröße) = floor(%d / %d)" % (
         blocknummer, adresse, blocksize))
+    print("Offset (in Bytes): %d\t\t\tBerechnung: Adresse mod Blockgröße = %d mod %d" % (offset, adresse, blocksize))
+    print("\nSpeziell für dieses Cache Level:")
     print("Anzahl Blöcke im Cache: %d\t\tBerechnung: Gesamtspeicherplatz / Blockgröße = %d / %d" % (
         anzahlbloecke, cachesize, blocksize))
     print("Anzahl Sets im Cache: %d\t\tBerechnung: Anzahl Blöcke / N = %d / %d" % (anzahlsets, anzahlbloecke, n))
@@ -47,7 +50,6 @@ def xWay():
         setindex, blocknummer, anzahlsets))
     print("Tag: %d\t\t\t\t\t\tBerechnung: floor(Blocknummer / Anzahl Sets) = floor(%d / %d)" % (
         tag, blocknummer, anzahlsets))
-    print("Offset (in Bytes): %d\t\t\tBerechnung: Adresse mod Blockgröße = %d mod %d" % (offset, adresse, blocksize))
 
 
 print("1: Direct Mapped Cache\n2: N-Way-Set-Associative Cache")
